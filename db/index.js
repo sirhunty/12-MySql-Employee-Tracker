e2a1d36166b80c00569e9fa6e7f8c7a6b0262da4
Hunter Matthews <sirhunty@gmail.com>
2020-04-18 11:31:31 -0700

    added connections to find all roles and to display dept name, create and remove roles from the db

--- db/index.js
+++ db/index.js
@@ -52,6 +52,22 @@ class DB {
     );
   }
 
+  // Find all roles, join with departments to display the department name
+  findAllRoles() {
+    return this.connection.query(
+      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+    );
+  }
+
+  // Create a new role
+  createRole(role) {
+    return this.connection.query("INSERT INTO role SET ?", role);
+  }
+
+  // Remove a role from the db
+  removeRole(roleId) {
+    return this.connection.query("DELETE FROM role WHERE id = ?", roleId);
+  }
 
 
 }
